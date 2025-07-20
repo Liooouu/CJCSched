5360dad6a3f2941bf54b7913752defa91d9b2bd4
Liooouu <apiadojohnliol@gmail.com>
2025-07-20 20:39:29 +0800

    Adding Login Form

--- frontend/cjc/components/Login.jsx
+++ frontend/cjc/components/Login.jsx
@@ -4,37 +4,44 @@ export const Login = () => {
     return(
     <>
       <LogNavbar/>
-       <div className="min-h-screen bg-gradient-to-l from-indigo-300 to-indigo-500">
-        <div className="grid grid-cols-1 md:grid-cols-2 gap-30 w-full max-w-5xl p-6">
+       <div className="min-h-screen flex items-center justify-center bg-gradient-to-l from-indigo-300 to-indigo-500">
+        <div className="grid grid-cols-1 md:grid-cols-2 gap- w-full max-w-5xl p-6">
 
             <div className="text-white flex flex-col justify-center">
                 <h1 className="text-3xl font-bold mb-6">Enter Your Account</h1>
                 <p className="mb-6">Please log in using your personal information to stay connected to us</p>
-                <div>
-                    <span>Connect with us:
+                <div className="text-sm flex flex-wrap items-center space-x-2">
+                    <span>Connect with us: </span>
                         <a 
-                        href="#" 
+                        href="https://www.facebook.com/cjcrsg" 
                         target="_blank"
-                        className="underline px-2"
+                        rel="noopener noreferrer"
+                        className="underline hover:text-blue-300"
                         >
                             Facebook
                         </a>
                         <a 
                         href="#" 
                         target="_blank"
-                        className="underline px-2"
+                        className="underline hover:text-blue-300"
                         >
                             Gmail
                         </a>
                         <a 
                         href="#" 
                         target="_blank"
-                        className="underline px-2"
+                        className="underline hover:text-blue-300"
                         >
                             Contact
                         </a>
-                    </span>
                 </div>
+            </div>
+
+            <div>
+                <h2>Login</h2>
+                <form>
+
+                </form>
             </div>
 
         </div> 
