3d24fe2b047d3f8d884b79e84ea6ac7f698bfb10
Liooouu <apiadojohnliol@gmail.com>
2025-07-28 00:54:37 +0800

    fixed minimal errors

--- frontend/cjc/pages/ResetPasswordPage.jsx
+++ frontend/cjc/pages/ResetPasswordPage.jsx
@@ -1,5 +1,5 @@
 import { Link } from "react-router-dom";
-import { ResetNavbar } from "../components/resetNavbar";
+import { ResetNavbar } from "../components/ResetNavbar";
 import{FaArrowLeft} from "react-icons/fa";
 
 export const ResetPassword = () => {
