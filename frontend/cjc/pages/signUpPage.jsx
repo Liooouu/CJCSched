66abefc6a1903dd02d4481864a2092d818486a17
gwynnnot <matolentino8@up.edu.ph>
2025-07-27 23:51:30 +0800

    ui: login and signup page

--- frontend/cjc/pages/signUpPage.jsx
+++ frontend/cjc/pages/signUpPage.jsx
@@ -11,14 +11,14 @@ export const Signup = () => {
     <>
     <SignNavbar/>
     <div className="min-h-screen flex items-center justify-center bg-gradient-to-r from-indigo-200 to-indigo-900 text-gray-800">
-      <div className="grid grid-cols-1 md:grid-cols-2 gap-30 w-full max-w-5xl p-6 font-montserrat">
+      <div className="grid grid-cols-1 md:grid-cols-2 gap-15 lg:gap-30 max-w-screen-xl p-6 font-montserrat">
         
-        <div className="text-white flex flex-col justify-center">
-          <h1 className="text-3xl font-bold mb-4">Create your account</h1>
-           <p className="mb-6">
+        <div className="text-white flex flex-col justify-center items-center text-center">
+          <h1 className="text-3xl lg:text-4xl font-bold mb-4">Create your account</h1>
+           <p className="text-xl lg:text-2xl mb-6">
             To become a part of our community, please sign up using your personal information.
            </p>
-          <div className="text-sm flex flex-wrap items-center space-x-2">
+          <div className="text-sm flex flex-wrap items-center space-x-2 mt-0 md:mt-64">
             <span>Connect with us:</span>
             <a
               href="https://www.facebook.com/cjcrsg"
@@ -41,24 +41,24 @@ export const Signup = () => {
           </div>
         </div>
 
-        <div className="bg-white rounded-xl shadow-lg p-8">
+        <div className="bg-white rounded-2xl shadow-lg p-8 w-full max-w-lg mx-auto">
           <h2 className="text-2xl font-semibold mb-6 text-center">Sign Up</h2>
 
-          <form className="space-y-6">
+          <form className="space-y-6 text-lg lg:text-xl">
             <input
               type="text"
               placeholder="Full Name (Ex: Juan Dela Cruz)"
-              className="w-full px-4 py-2 border rounded-md"
+              className="w-full px-4 py-2 border rounded-xl"
             />
             <input
               type="text"
               placeholder="Contact No."
-              className="w-full px-4 py-2 border rounded-md"
+              className="w-full px-4 py-2 border rounded-xl"
             />
             <input
               type="text"
               placeholder="Address"
-              className="w-full px-4 py-2 border rounded-md"
+              className="w-full px-4 py-2 border rounded-xl"
             />
 
             <div className="flex items-center space-x-4">
@@ -77,14 +77,14 @@ export const Signup = () => {
             <input
               type="email"
               placeholder="Email"
-              className="w-full px-4 py-2 border rounded-md"
+              className="w-full px-4 py-2 border rounded-xl"
             />
 
             <div className="relative">
               <input
                 type={showPassword ? "text" : "password"}
                 placeholder="Password"
-                className="w-full px-4 py-2 border rounded-md"
+                className="w-full px-4 py-2 border rounded-xl"
               />
               <button
                 type="button"
@@ -97,13 +97,13 @@ export const Signup = () => {
 
             <button
               type="submit"
-              className="w-full bg-indigo-200 hover:bg-indigo-500 text-indigo-900 font-semibold py-2 rounded-md"
+              className="w-full bg-indigo-200 hover:bg-indigo-500 text-indigo-900 font-semibold py-2 rounded-xl"
             >
               Sign Up
             </button>
           </form>
 
-          <p className="mt-4 text-sm text-center">
+          <p className="mt-4 text-sm lg:text-lg text-center">
             Already have an account?{""}
             <a className="text-indigo-600 hover:underline px-1">
               <Link to="/login">Log in</Link>
