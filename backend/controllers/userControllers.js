113aac4aafba75750c7b29c11ad7219441e159a6
Andrei <andreimanacop1@gmail.com>
2025-08-01 08:17:32 +0800

    adding crud operation in backend.

--- backend/controllers/userControllers.js
+++ backend/controllers/userControllers.js
@@ -1,19 +1,46 @@
-const express = require('express')
-const router = express.Router()
+
+let createdNewUsers = []
+let newUsersId = [1]
+
 
 const getUsers = (req,res)=> {
-    res.status(200).json({message:"thanks for logging in"})
+    const id = req.body.params
+    const usersDatabase = createdNewUsers.findOne(e => e.id === id)
+    res.status(200).json(usersDatabase)
 }
 const getAllUsers = (req,res)=> {
-    res.status(200).json({message:"get all users"})
+    res.status(200).json(createdNewUsers)
 }
 const createUsers = (req,res)=> {
-    res.status(200).json({message:"create users"})
+    const {name, email, password} = req.body
+
+    try{
+        if(!name || !email || !password){
+            throw new Error("Fill up the missing fields!");
+        }
+
+        const newUsers = {
+            id: newUsersId++,
+            name, 
+            email, 
+            password
+        }
+
+        createdNewUsers.push(newUsers)
+        res.status(200).json({message: "users succesfully created", user:newUsers})
+
+    }catch(e){
+        res.status(404).json({message: "failed to create", error:e.message})
+    }
 }
 const updateUsers = (req,res)=> {
+    const id = req.body.params
+    createdNewUsers.deleteOne(e => e.id === id)
     res.status(200).json({message:"updating the users"})
 }
 const deleteUsers = (req,res)=> {
+    const id = req.body.params
+    createdNewUsers.deleteOne(e => e.id === id)
     res.status(200).json({message:"delete users"})
 }
 
