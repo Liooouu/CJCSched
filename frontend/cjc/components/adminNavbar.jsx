c51d945d96594cdcb0e9bdcac016dedc30cd246c
gwynnnot <matolentino8@up.edu.ph>
2025-08-11 16:59:43 +0800

    ui: make navbar sticky and change hover effect to sliding underline

--- frontend/cjc/components/adminNavbar.jsx
+++ frontend/cjc/components/adminNavbar.jsx
@@ -4,7 +4,7 @@ import { Link } from 'react-router-dom';
 
 const adminNavbar = () => {
   return (
-    <nav>
+    <nav className="sticky top-0 z-50 shadow-md">
         <div className='flex flex-row p-2 md:text-lg lg:text-xl text-[#364687] bg-gradient-to-r from-[#364687]/0 to-[#EAEDF7]/100 '>
             <div className='container flex flex-row justify-center items-center gap-2 flex-1/3 ' >
                 <img src="/LOGO.png" alt="CJCRSG LOGO" className='w-[50px] h-[50px]'/>
