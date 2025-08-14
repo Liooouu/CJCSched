4c3614f8b10c53a0445e8c77276fd6c34556db00
John Lio Apiado <s2023101518@firstasia.edu.ph>
2025-08-14 18:24:08 +0800

    Remove files no longer needed in Gwy

--- backend/routes/userRoutes.js
+++
@@ -1,9 +0,0 @@
-const express = require('express')
-const router = express.Router()
-const {getAllUsers,getUsers,updateUsers,deleteUsers,createUsers} = require('../controllers/userControllers')
-
-
-router.route('/').get(getAllUsers).post(createUsers)
-router.route('/:id').put(updateUsers).delete(deleteUsers).get(getUsers)
-
-module.exports = router
