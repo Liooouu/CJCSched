fdb5080875fd4ba08ecc1afc802207f7cfdbe9e5
John Lio Apiado <s2023101518@firstasia.edu.ph>
2025-08-14 18:16:37 +0800

    WIP: admin navbar changes

--- frontend/cjc/components/adminNavbar.jsx
+++ frontend/cjc/components/adminNavbar.jsx
@@ -1,6 +1,6 @@
 import { Link, useNavigate } from "react-router-dom";
 import { useEffect, useState } from "react";
-
+/**Fixing */
 const AdminNavbar = () => {
   const navigate = useNavigate();
   const [isLoggedIn, setIsLoggedIn] = useState(false);
