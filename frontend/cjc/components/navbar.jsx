9eceba23dc41c97a23a554f61d11cf757ae7edfd
Liooouu <apiadojohnliol@gmail.com>
2025-08-20 14:25:55 +0800

    Fix: Fixed Responsiveness of navbar into hamburger navbar on mobile view

--- frontend/cjc/components/navbar.jsx
+++ frontend/cjc/components/navbar.jsx
@@ -2,7 +2,7 @@ import { use, useState } from 'react';
 import { Link } from 'react-router-dom';
 import { Menu, X } from 'lucide-react';
 
-/*Fixed*/
+/*hamburger Navbar*/
 const Navbar = () => {
   
   const [isOpen, setIsOpen] = useState(false);
